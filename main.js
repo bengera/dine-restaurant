5b99d865ec83673021858ebc2a69b79dc482a1d3
Ben <61493854+bengera@users.noreply.github.com>
2025-03-06 14:15:15 +0700

    Align border unnderline on second event button and add minor functionality to event buttons

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log('Testing line for JavaScript');
+const eventButtons = document.querySelectorAll('.events__event-type-button');
+
+eventButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        eventButtons.forEach((button) => {
+            button.classList.remove('active')
+        })
+        button.classList.add('active')
+    })
+    
+})
+
