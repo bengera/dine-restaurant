0340974933874098ae78781457e318040e2fe916
Ben <61493854+bengera@users.noreply.github.com>
2025-03-14 09:20:17 +0700

    Add Prettier formatting

--- images.js
+++ images.js
@@ -1,25 +1,24 @@
 // Directly import all images, Vite doesn't get dynamically inserted images
-import socialEventsDesktop from './images/homepage/social-events-desktop.jpg';
-import socialEventsDesktop2x from './images/homepage/social-events-desktop@2x.jpg';
-import socialEventsMobile from './images/homepage/social-events-mobile.jpg';
-import socialEventsMobile2x from './images/homepage/social-events-mobile@2x.jpg';
-import socialEventsTablet from './images/homepage/social-events-tablet.jpg';
-import socialEventsTablet2x from './images/homepage/social-events-tablet@2x.jpg';
+import socialEventsDesktop from "./images/homepage/social-events-desktop.jpg";
+import socialEventsDesktop2x from "./images/homepage/social-events-desktop@2x.jpg";
+import socialEventsMobile from "./images/homepage/social-events-mobile.jpg";
+import socialEventsMobile2x from "./images/homepage/social-events-mobile@2x.jpg";
+import socialEventsTablet from "./images/homepage/social-events-tablet.jpg";
+import socialEventsTablet2x from "./images/homepage/social-events-tablet@2x.jpg";
 
-import specialEventsDesktop from './images/homepage/special-events-desktop.jpg';
-import specialEventsDesktop2x from './images/homepage/special-events-desktop@2x.jpg';
-import specialEventsMobile from './images/homepage/special-events-mobile.jpg';
-import specialEventsMobile2x from './images/homepage/special-events-mobile@2x.jpg';
-import specialEventsTablet from './images/homepage/special-events-tablet.jpg';
-import specialEventsTablet2x from './images/homepage/special-events-tablet@2x.jpg';
+import specialEventsDesktop from "./images/homepage/special-events-desktop.jpg";
+import specialEventsDesktop2x from "./images/homepage/special-events-desktop@2x.jpg";
+import specialEventsMobile from "./images/homepage/special-events-mobile.jpg";
+import specialEventsMobile2x from "./images/homepage/special-events-mobile@2x.jpg";
+import specialEventsTablet from "./images/homepage/special-events-tablet.jpg";
+import specialEventsTablet2x from "./images/homepage/special-events-tablet@2x.jpg";
 
-import familyGatheringDesktop from './images/homepage/family-gathering-desktop.jpg';
-import familyGatheringDesktop2x from './images/homepage/family-gathering-desktop@2x.jpg';
-import familyGatheringMobile from './images/homepage/family-gathering-mobile.jpg';
-import familyGatheringMobile2x from './images/homepage/family-gathering-mobile@2x.jpg';
-import familyGatheringTablet from './images/homepage/family-gathering-tablet.jpg';
-import familyGatheringTablet2x from './images/homepage/family-gathering-tablet@2x.jpg';
-
+import familyGatheringDesktop from "./images/homepage/family-gathering-desktop.jpg";
+import familyGatheringDesktop2x from "./images/homepage/family-gathering-desktop@2x.jpg";
+import familyGatheringMobile from "./images/homepage/family-gathering-mobile.jpg";
+import familyGatheringMobile2x from "./images/homepage/family-gathering-mobile@2x.jpg";
+import familyGatheringTablet from "./images/homepage/family-gathering-tablet.jpg";
+import familyGatheringTablet2x from "./images/homepage/family-gathering-tablet@2x.jpg";
 
 export const images = {
   "social-events-desktop": socialEventsDesktop,
