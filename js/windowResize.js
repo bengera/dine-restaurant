e48481e0a8d09205e523219f81dc9c6add60b76e
Ben <61493854+bengera@users.noreply.github.com>
2025-09-25 09:16:59 +0700

    Add date legend for tablet

--- js/windowResize.js
+++ js/windowResize.js
@@ -1,5 +1,7 @@
 const timeDesMobile = document.getElementById("description-mobile");
 const timeDesTablet = document.getElementById("description-tablet");
+const datesDesMobile = document.querySelector(".dates__description");
+const datesDesTablet = document.querySelector(".dates__description.mobile");
 
 (function () {
   window.onresize = displayWindowSize;
@@ -12,9 +14,13 @@ const timeDesTablet = document.getElementById("description-tablet");
     if (myWidth >= 480) {
       timeDesMobile.style.display = "none";
       timeDesTablet.style.display = "block";
+      datesDesMobile.style.display = "none";
+      datesDesTablet.style.display = "block";
     } else {
       timeDesMobile.style.display = "block";
       timeDesTablet.style.display = "none";
+      datesDesMobile.style.display = "block";
+      datesDesTablet.style.display = "none";
     }
   }
 })();
